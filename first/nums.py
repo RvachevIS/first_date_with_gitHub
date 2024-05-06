4402e91e32e9e158bf1becc86c4d51b14308c4da
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-06 21:29:37 +0300

    Добавлена функция vir(a, b)

--- first/nums.py
+++ first/nums.py
@@ -7,6 +7,9 @@ def minus(a, b):
 def vir(a, b):
     return 2*(a+b)
 
+def abcd(a, b):
+    return 2*(a-b)
+
 def mul(a, b):
     return a*b
 
@@ -15,3 +18,4 @@ if __name__ == '__main__':
     print(minus(4, 2))
     print(mul(2, 4))
     print(vir(3, 2))
+    print(abcd(3, 1))

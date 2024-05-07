c06ac94f722bb1e08896a99c627ce7db6b4c94ab
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-07 23:27:26 +0300

    в модуле heads_tails_game.py дополнен и видоизменен код на подсчет среднего количества игр среди 10 человек

--- while_traning/heads_tails_game.py
+++ while_traning/heads_tails_game.py
@@ -1,5 +1,6 @@
 import random
 
+# 100 игроков, узнаем какое среднее количество ходов для поражения
 HEADS = "heads"
 TAILS = "tails"
 COIN_VALUES = [HEADS, TAILS]
@@ -9,25 +10,25 @@ def flip_coin():
     return random.choice(COIN_VALUES)
 
 
-def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int) -> int:
+def play_martingale(*,
+                    starting_funds: int,
+                    min_bet: int,
+                    max_bet: int
+                    ) -> int:
     steps_to_loose = 0
     current_funds = starting_funds
     current_bet = min_bet
 
     while current_funds > 0:
-        print('=======')
         steps_to_loose += 1
         current_funds -= current_bet
-        print(f"{current_funds=}, {current_bet=}")
         flipped_coin_value = flip_coin()
 
         if flipped_coin_value == HEADS:
             win = current_bet * 2
-            print(f"{win=}")
             current_funds += win
             current_bet = min_bet
         else:
-            print(f"loose")
             current_bet *= 2
             if current_bet > max_bet:
                 current_bet = min_bet
@@ -36,5 +37,25 @@ def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int) -> int:
     return steps_to_loose
 
 
+def simulate_martigale_for_n_players(*,
+                                     starting_funds: int,
+                                     min_bet: int,
+                                     max_bet: int,
+                                     n_games: int
+                                     ) -> float:
+    total_steps_to_loose = 0
+    for i in range(n_games):
+        step_to_loose = play_martingale(starting_funds=starting_funds,
+                                        min_bet=min_bet,
+                                        max_bet=max_bet
+                                        )
+        total_steps_to_loose += step_to_loose
+    return total_steps_to_loose / n_games
+
 if __name__ == '__main__':
-    print(play_martingale(starting_funds=100, min_bet=1, max_bet=100))
+    print(simulate_martigale_for_n_players(n_games=10,
+                                           starting_funds=1000,
+                                           min_bet=1,
+                                           max_bet=100)
+                                            )
+#СРЕДНЕЕ КОЛИЧЕСТВО ИГР

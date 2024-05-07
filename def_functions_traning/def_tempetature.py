774d9ce17ff94a039db90e92bd6649b1858f7355
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-07 21:18:31 +0300

    Добавлен модуль def_tempetature.py и наполнен кодом

---
+++ def_functions_traning/def_tempetature.py
@@ -0,0 +1,9 @@
+COMFORTABLE_TEMPERATURE = 25
+
+
+def get_diff_from_comfortable_temperature(*, temperature: int) -> int:
+    return COMFORTABLE_TEMPERATURE - temperature
+
+
+if __name__ == '__main__':
+    print(get_diff_from_comfortable_temperature(temperature=20))

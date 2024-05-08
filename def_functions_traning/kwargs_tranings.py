786883be2a4bab91af1efefbd1b6d85bdf65da64
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-08 22:33:16 +0300

    kwargs_tranings.py второй пример кода с кваргами - функция с любыми аргументами

--- def_functions_traning/kwargs_tranings.py
+++ def_functions_traning/kwargs_tranings.py
@@ -1,11 +1,20 @@
-def introduce(**kwargs):
-    for key, value in kwargs.items():
-        print(key)
-        print(value)
-
+def func_with_all_arguments(
+                            x: int,
+                            y: int,
+                            *args,
+                            value: int = 6,
+                            **kwargs
+):
+    print(x, y)
+    print(args)
+    print(value)
+    print(kwargs)
 person = {
-    "city": "New York",
-    "age": 31,
-    "name": "Ivan"
+    "city": "Vladimir",
+    "first_name": "Ivan",
+    "last_name": "Rvachev",
 }
-introduce(**person)
+func_with_all_arguments(1,
+                        2,
+                        3, 4, 5,
+                        **person)

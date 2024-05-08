74010ec7acd077dcd66d352f7e6c6a382e1dc4dd
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-09 01:22:41 +0300

    в папке import_traning создали модуль e_operations_import.py добавил ссылки на модуль easy_operations.py

---
+++ import_what_is_it/e_operations_import.py
@@ -0,0 +1,10 @@
+# from import_what_is_it.easy_operations import add, subtract
+from import_what_is_it import easy_operations
+# from import_what_is_it import *
+
+
+if __name__ == '__main__':
+#    print(add(3, 1))
+#    print(subtract(3, 8))
+    print(easy_operations.add(3, 9))
+    print(easy_operations.subtract(3, 18))

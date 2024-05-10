070efaf44566a18e35dfb303bc1ace02138aa2fa
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-10 18:42:16 +0300

    в модуле new_class_test.py показан наглядный пример боя 2ух существ и при изменении их лвл бой показывает различный результат, далее булет бой по сложнее с доп фичами

--- class_testing/new_class_test.py
+++ class_testing/new_class_test.py
@@ -5,36 +5,43 @@ class Character:
         self.attack_power = self.base_attack_power * level
 
 
-    def attack(self):
-        print(f"{self.character_name}"
-              f" attacks with {self.attack_power} power")
+    def attack(self, *, target: "Character") -> None:
+        print(
+            f"{self.character_name} attacks {target.character_name} ({target.health_points} health_points) "
+            f"with {self.attack_power} power"
+        )
+        target.health_points -= self.attack_power
+        print(f"After attack {self.character_name} hp has {target.health_points}")
+
+
+    def is_alive(self) -> bool:
+        return self.health_points > 0
 
 
     def __str__(self) -> str:
         return (f"{self.character_name} (level: {self.level}"
-                f" hp: {self.health_points})")
+                f", hp: {self.health_points})")
 
 
 class Ork(Character):
     base_health_points = 100
     base_attack_power = 10
     character_name = "Ork"
-
-
 class Elf(Character):
     base_health_points = 50
     base_attack_power = 15
     character_name = "Elf"
 
 
-    def attack(self):
-        print("This method is from class-inheritor")
+def fight(*, character_1: Character, character_2: Character) -> None:
+    while character_1.is_alive() and character_2.is_alive():
+        character_1.attack(target=character_2)
+        if character_2.is_alive():
+            character_2.attack(target=character_1)
 
-ork1 = Ork(level=1)
-elf1 = Elf(level=3)
+    print(f"Character 1: {character_1}, is alive: {character_1.is_alive()}")
+    print(f"Character 2: {character_2}, is alive: {character_2.is_alive()}")
 
-if __name__ == '__main__':
-    ork1.attack()
-    print(ork1)
-    elf1.attack()
-    print(elf1)
+ork = Ork(level=15)
+elf = Elf(level=19)
+fight(character_1=ork, character_2=elf)

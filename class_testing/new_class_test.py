299d1ad4f684c82bb7032efadb7d09c04f28ddd8
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-10 18:01:35 +0300

    модуль new_class_test.py с классном class Ork дополнил новым классном идентичным этому, но действует правило (dont repeat yourself) код добавлен в чисто учебных целях

--- class_testing/new_class_test.py
+++ class_testing/new_class_test.py
@@ -16,7 +16,22 @@ class Ork:
 ork = Ork(level=2)
 
 
+class Elf:
+    def __init__(self, level: int) -> None:
+        self.level = level
+        self.healthpoint = 50 * level
+        self.attack_power = 15 * level
+
+
+    def attack(self):
+        print(f"Elf attacks with {self.attack_power} power")
+
+
+    def __str__(self):
+        return f"Elf (level: {self.level} hp: {self.healthpoint})"
+
+
 if __name__ == '__main__':
 #    print(ork.attack_power)
 #    ork.attack()
-    print(ork)
+#    print(ork)

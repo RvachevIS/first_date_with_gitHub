b19c73d8f23abea7a1b1ae67d642849d13c0aa2f
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-10 18:14:33 +0300

    в модуле new_class_test.py создали 2ух персонажей, эльф и орк, так скажем код подготовленный для боя между ними, в следующем коде будет продемонстрирован их бой

--- class_testing/new_class_test.py
+++ class_testing/new_class_test.py
@@ -1,37 +1,40 @@
-class Ork:
-    def __init__(self, level: int) -> None:
+class Character:
+    def __init__(self, *, level: int) -> None:
         self.level = level
-        self.healthpoints = 100 * level
-        self.attack_power = 100 * level
+        self.health_points = self.base_health_points * level
+        self.attack_power = self.base_attack_power * level
 
 
     def attack(self):
-        print(f"Ork attacks with {self.attack_power} power")
+        print(f"{self.character_name}"
+              f" attacks with {self.attack_power} power")
 
 
-    def __str__(self):
-        return f"Ork (level: {self.level} hp: {self.healthpoints})"
+    def __str__(self) -> str:
+        return (f"{self.character_name} (level: {self.level}"
+                f" hp: {self.health_points})")
 
 
-ork = Ork(level=2)
+class Ork(Character):
+    base_health_points = 100
+    base_attack_power = 10
+    character_name = "Ork"
 
 
-class Elf:
-    def __init__(self, level: int) -> None:
-        self.level = level
-        self.healthpoint = 50 * level
-        self.attack_power = 15 * level
+class Elf(Character):
+    base_health_points = 50
+    base_attack_power = 15
+    character_name = "Elf"
 
 
     def attack(self):
-        print(f"Elf attacks with {self.attack_power} power")
+        print("This method is from class-inheritor")
 
-
-    def __str__(self):
-        return f"Elf (level: {self.level} hp: {self.healthpoint})"
-
+ork1 = Ork(level=1)
+elf1 = Elf(level=3)
 
 if __name__ == '__main__':
-#    print(ork.attack_power)
-#    ork.attack()
-#    print(ork)
+    ork1.attack()
+    print(ork1)
+    elf1.attack()
+    print(elf1)

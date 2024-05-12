595ca4b676a265ea6442767315e1c449558124fe
Ivan_Rvachev <norwegianblack@mail.ru>
2024-05-12 18:41:42 +0300

    изменил код в модуле file_traning.py  с создания файла на его загрузку/просмотр

--- files_tranings/file_traning.py
+++ files_tranings/file_traning.py
@@ -1,7 +1,6 @@
 import json
 
-data = {"name": "Ivan", "age": 31, "city": "Vladimir"}
-file = open("files/data.json", "w")
-json.dump(data, file, indent=4)
-
+file = open("files/data.json", "r")
+loaded_data = json.load(file)
 file.close()
+print(loaded_data)
